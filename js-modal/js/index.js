ad7f7a8a772f12cf985cad45a5f2e98a68441f2f
Aasha Bhandari <bhandariaasha700@gmail.com>
2020-10-13 16:37:23 +0545

    added on escape/mouse-click outside modal close functionality

--- js-modal/js/index.js
+++ js-modal/js/index.js
@@ -1,9 +1,20 @@
 
-        var modal = document.getElementById('modal');
-        var close = document.getElementsByClassName('close');
-        function myFunction() {
-            modal.style.display = 'block';
-        }
-        function closeModal() {
-            modal.style.display = 'none';
-        }
+var modal = document.getElementById('modal');
+var close = document.getElementsByClassName('close');
+function myFunction() {
+    modal.style.display = 'block';
+}
+function closeModal() {
+    modal.style.display = 'none';
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+document.onkeydown = function (evt) {
+    if (evt && evt.key === "Escape" || evt && evt.key === "Esc") {
+        modal.style.display = "none"
+
+    }
+}
