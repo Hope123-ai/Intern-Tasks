86b40372a13e04dee444f8d0bdcb62e554100316
Aasha Bhandari <bhandariaasha700@gmail.com>
2020-10-13 11:20:09 +0545

    finished js-modal task

---
+++ js-modal/js/index.js
@@ -0,0 +1,9 @@
+
+        var modal = document.getElementById('modal');
+        var close = document.getElementsByClassName('close');
+        function myFunction() {
+            modal.style.display = 'block';
+        }
+        function closeModal() {
+            modal.style.display = 'none';
+        }
