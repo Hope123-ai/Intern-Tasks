6b32e6363e4af2d2c4b186c91a4f70c100d21acc
Aasha Bhandari <bhandariaasha700@gmail.com>
2020-10-20 09:21:33 +0545

    added onhover stop feature

--- js-slider/js/index.js
+++ js-slider/js/index.js
@@ -1,4 +1,4 @@
-var images = document.getElementsByClassName("images__items");  
+var images = document.getElementsByClassName("images__items");   
 console.log(images);
 
 function slider(images, interval) {
@@ -8,32 +8,48 @@ function slider(images, interval) {
   this.position = 0;
   this.changeInterval = 50;
   this.Index = 1;
-  this.changeImage = () => {
+  this.stop = false; 
+
+   this.changeImage = () => {
     setInterval(() => {
-      this.currentIndex++;
-      for (let i = 0; i <= 10 * this.currentIndex; i++) {
-        let rt = this;
-        (function (ind) {
-          setTimeout(function () {
-            if (i === 0) {
-              rt.images[rt.currentIndex].style.left = -(10 * 1) + '%';
-            } else {
-              rt.images[rt.currentIndex].style.left = -(10 * i) + '%';
-            }
-          }, 50 * (ind + 1));
-        })(i);
-      }
-      if (this.currentIndex >= this.images.length) {
-        this.currentIndex = 0;
-        this.position = 0;
-        for (let i = 0; i < this.images.length; i++) {
-          this.images[i].style.left = 0;
+      if (!this.stop) {
+        this.currentIndex++;
+        for (let i = 0; i <= 10 * this.currentIndex; i++) {
+          let rt = this;
+          (function (ind) {
+            setTimeout(function () {
+              if (i === 0) {
+                rt.images[rt.currentIndex].style.left = -(10 * 1) + '%';
+              } else {
+                rt.images[rt.currentIndex].style.left = -(10 * i) + '%';
+              }
+            }, 50 * (ind + 1));
+          })(i);
         }
-      }
+        if (this.currentIndex >= this.images.length) {
+          this.currentIndex = 0;
+          this.position = 0;
+          for (let i = 0; i < this.images.length; i++) {
+            this.images[i].style.left = 0;
+          }
+        }
+      }      
     }, this.interval);
   };
+
+  this.overset = () => {
+    let rt = this;
+    for (let i = 0; i < this.images.length; i++) {
+    this.images[i].onmouseover = function(){
+      rt.stop = true
+    };
+    this.images[i].onmouseout = function(){
+      rt.stop = false
+    };
+  }
+}
 }
 var slider = new slider(images, 2000);
-console.log(slider);
+slider.overset();
 slider.changeImage();
-
+console.log(slider);
